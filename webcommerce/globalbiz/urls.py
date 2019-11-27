7bcc44b76606f2ce5b150c4e6a7b1ad78ffc9a16
BabGee <gjeremiah170@gmail.com>
2019-11-27 17:18:21 +0300

    Added Category Routes and templates

--- webcommerce/globalbiz/urls.py
+++ webcommerce/globalbiz/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #pages path
     path('', views.home, name='globalbiz-home'),
+    #products path
     path('Macbook Pro New', views.macbook, name='globalbiz-mac'),
     path('Jeep New Model 2019', views.jeep, name='globalbiz-jeep'),
+    #categories path
+    path('Electronics', views.electronics, name='globalbiz-electronics'),
+    path('Furniture', views.furniture, name='globalbiz-furniture'),
 ]
--- webcommerce/globalbiz/views.py
+++ webcommerce/globalbiz/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from category.models import Category
 from product.models import Product
 
+
+
+#******************pages.html ***********************************
 def home(request):
     context = {
         'products' : Product.objects.all(), 
@@ -11,6 +14,8 @@ def home(request):
     }
     return render(request, 'globalbiz/index.html', context)
 
+
+#****************** product details.html ***********************************
 
 def macbook(request):
     context = {
@@ -25,3 +30,21 @@ def jeep(request):
     }
    
     return render(request, 'globalbiz/products/jeep.html', context)
+
+#****************** Categories.html ***********************************
+
+def electronics(request):
+    context = {
+        'newElectronics' : Product.objects.filter(category__name='Electronics', rating__name='New'),
+        'offerElectronics' : Product.objects.filter(category__name='Electronics', rating__name='Offer')
+    }
+   
+    return render(request, 'globalbiz/category/electronics.html', context)
+
+def furniture(request):
+    context = {
+        'newFurn' : Product.objects.filter(category__name='Furniture', rating__name='New'),
+        'offerFurn' : Product.objects.filter(category__name='Furniture', rating__name='Offer')
+    }
+   
+    return render(request, 'globalbiz/category/furniture.html', context)
