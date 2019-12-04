a3e7f5b7fe1d9db0a00416868d556d993cbe8987
BabGee <gjeremiah170@gmail.com>
2019-12-04 12:33:00 +0300

    added cart count

---
+++ webcommerce/globalbiz/templatetags/cart_template_tags.py
@@ -0,0 +1,13 @@
+from django import template
+from product.models import Order
+
+register = template.Library()
+
+
+@register.filter
+def cart_product_count(user):
+    if user.is_authenticated:
+        qs = Order.objects.filter(user=user, ordered=False)
+        if qs.exists():
+            return qs[0].products.count()
+    return 0
