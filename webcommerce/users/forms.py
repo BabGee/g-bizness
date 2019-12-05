4a7fb52576aea1ee05bc4bfd36d63f73ab95d96c
BabGee <gjeremiah170@gmail.com>
2019-12-05 14:20:48 +0300

    updated dependencies

--- webcommerce/users/forms.py
+++ webcommerce/users/forms.py
@@ -1,6 +1,29 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+# from django_countries.fields import CountryField
+# from django_countries.widgets import CountrySelectWidget
+
+PAYMENT_CHOICES = (
+    ('M', 'MPESA'),
+    ('P', 'PayPal')
+)
+
+DELIVERY_CHOICES = (
+    ('S', 'Pick up Station'),
+    ('H', 'Home or Office')
+)
+
+STATION_CHOICES = (
+    ('CB', 'NAIROBI CBD'),
+    ('WS', 'WESTLANDS'),
+	('JG', 'UCHUMI JOGOO ROAD'),
+	('GC', 'GARDEN CITY'),
+	('MS', 'MOMBASA CBD'),
+	('KS', 'KISUMU CBD'),
+	('NK', 'NAKURU'),
+)	
+
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
@@ -9,3 +32,25 @@ class UserRegisterForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ["username", "phone", "email", "password1", "password2"]
+
+
+
+# class CheckoutForm(forms.Form):
+#     first_name = forms.CharField(required=True)
+# 	last_name = forms.CharField(required=True)
+# 	mobi_number = forms.IntegerField(required=True)
+#     delivery_address = forms.CharField(required=False)
+#     shipping_station =forms.CharField(label='Pick up station', widget=forms.Select(choices=STATION_CHOICES))
+# 	delivery_option = forms.ChoiceField(
+#         widget=forms.RadioSelect, choices=DELIVERY_CHOICES)
+#     payment_option = forms.ChoiceField(
+#         widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+
+# class CouponForm(forms.Form):
+#     code = forms.CharField(widget=forms.TextInput(attrs={
+#         'class': 'form-control',
+#         'placeholder': 'Promo code',
+#         'aria-label': 'Recipient\'s username',
+#         'aria-describedby': 'basic-addon2'
+#     }))
--- webcommerce/users/models.py
+++ webcommerce/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# class Address(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+# 	mobi_number = models.IntegerField(max_length=10)
+#     delivery_address = models.CharField(max_length=100)
+#     shipping_station = models.CharField(max_length=100)
+#     default = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return self.user.username
+
+#     class Meta:
+#         verbose_name_plural = 'Addresses'
+
